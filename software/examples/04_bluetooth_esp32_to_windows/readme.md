3e0408363721833a3319880fc3668a9eb42d6841
Morgan Kenyon <morganwkenyon@gmail.com>
2024-05-25 22:10:05 -0500

    Got notifications flowing from esp32 to windows, still work to do

--- software/examples/04_bluetooth_esp32_to_windows/readme.md
+++ software/examples/04_bluetooth_esp32_to_windows/readme.md
@@ -1,3 +1,20 @@
 # Esp32 to Windows Bluetooth Example
 
 This project documents my attempt to push data from the Esp32 to my windows laptop.
+
+
+## Life!!!
+
+We have communication!!
+
+![bluetooth communication](image.png)
+
+## Issues
+
+Disconnect not working as expected:
+
+https://github.com/hbldh/bleak/pull/974
+
+https://github.com/hbldh/bleak/issues/807
+
+Bleak docs: https://bleak.readthedocs.io/en/latest/api/client.html
--- software/examples/04_bluetooth_esp32_to_windows/server_esp32/server_esp32.ino
+++ software/examples/04_bluetooth_esp32_to_windows/server_esp32/server_esp32.ino
@@ -18,6 +18,7 @@
 #include <BLE2902.h>
 #include <Wire.h>
 
+
 //BLE server name
 #define bleServerName "ESP32_Server"
 
@@ -25,18 +26,34 @@ float num;
 
 // Timer variables
 unsigned long lastTime = 0;
-unsigned long timerDelay = 10000;
+unsigned long lastWriteOperation = 0;
+unsigned long timerDelay = 3000;
 
 bool deviceConnected = false;
+bool oldDeviceConnected = false;//added
 
 // See the following for generating UUIDs:
 // https://www.uuidgenerator.net/
 #define SERVICE_UUID "91bad492-b950-4226-aa2b-4ede9fa42f59"
 
+BLEServer *pServer = NULL;//added
+BLEService *bmeService = NULL;
+
 // Number Characteristic and Descriptor
-BLECharacteristic bmeNumberCharacteristics("cba1d466-344c-4be3-ab3f-189f80dd7518", BLECharacteristic::PROPERTY_NOTIFY);
+BLECharacteristic bmeNumberCharacteristics(
+  "cba1d466-344c-4be3-ab3f-189f80dd7518", 
+  BLECharacteristic::PROPERTY_NOTIFY | 
+  BLECharacteristic::PROPERTY_WRITE);
 BLEDescriptor bmeNumberDescriptor(BLEUUID((uint16_t)0x2902));
 
+//https://stackoverflow.com/a/75943324
+static void my_gatts_event_handler(esp_gatts_cb_event_t event, esp_gatt_if_t gatts_if, esp_ble_gatts_cb_param_t* param) {
+    // Do stuff depending on event type
+    // Eg. 'listen' to GATT events in realtime (very star trek).
+    // tone(2000 + event*200, volume, delay);
+    
+    Serial.println("event");
+}
 
 //Setup callbacks onConnect and onDisconnect
 class MyServerCallbacks: public BLEServerCallbacks {
@@ -50,6 +67,32 @@ class MyServerCallbacks: public BLEServerCallbacks {
   }
 };
 
+class CharacterCallbacks: public BLECharacteristicCallbacks {
+  void onWrite(BLECharacteristic* pCharacteristic) {
+      std::string rxValue = pCharacteristic->getValue();
+      Serial.print("value received = ");
+      Serial.println(rxValue.c_str());
+      lastWriteOperation = millis();
+  }
+};
+
+//https://forum.arduino.cc/t/esp32-ble-does-not-disconnect/957235
+void checkToReconnect() //added
+{
+  // disconnected so advertise
+  if (!deviceConnected && oldDeviceConnected) {
+    delay(500); // give the bluetooth stack the chance to get things ready
+    pServer->startAdvertising(); // restart advertising
+    Serial.println("Disconnected: start advertising");
+    oldDeviceConnected = deviceConnected;
+  }
+  // connected so reset boolean control
+  if (deviceConnected && !oldDeviceConnected) {
+    // do stuff here on connecting
+    Serial.println("Reconnected");
+    oldDeviceConnected = deviceConnected;
+  }
+}
 
 void setup() {
   // Start serial communication 
@@ -57,11 +100,13 @@ void setup() {
 
   // Init BME Sensor
 
-  // Create the BLE Device
+  // Create the BLE Device\
+  BLEDevice::setCustomGattsHandler(my_gatts_event_handler);
   BLEDevice::init(bleServerName);
 
   // Create the BLE Server
   BLEServer *pServer = BLEDevice::createServer();
+  //*pServer = BLEDevice::createServer();
   pServer->setCallbacks(new MyServerCallbacks());
 
   // Create the BLE Service
@@ -72,6 +117,7 @@ void setup() {
   bmeService->addCharacteristic(&bmeNumberCharacteristics);
   bmeNumberDescriptor.setValue("BME Number");
   bmeNumberCharacteristics.addDescriptor(&bmeNumberDescriptor);
+  bmeNumberCharacteristics.setCallbacks(new CharacterCallbacks());
 
   
   // Start the service
@@ -85,8 +131,11 @@ void setup() {
 }
 
 void loop() {
+  //checkToReconnect();
+  //uint32_t connectedCount = pServer->getConnectedCount();
+  //Serial.println((unsigned)connectedCount);
+  
   if (deviceConnected) {
-    if ((millis() - lastTime) > timerDelay) {
       num = num + 1;
   
       //Notify number reading
@@ -100,6 +149,15 @@ void loop() {
       
       
       lastTime = millis();
-    }
   }
+
+  if (deviceConnected && ((millis() - lastWriteOperation) > 20000)) {
+    bmeService->stop();
+    bmeService->start();
+    BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+    pAdvertising->addServiceUUID(SERVICE_UUID);
+    pServer->getAdvertising()->start();
+    deviceConnected = false;
+  }
+  delay(1000);
 }
