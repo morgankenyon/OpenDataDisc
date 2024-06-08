fe92e9d1379d3666c5860faabca4a379e9171a25
Morgan Kenyon <morganwkenyon@gmail.com>
2024-06-07 21:58:05 -0500

    Cleaning up esp32 code

--- software/examples/08_bluetooth_with_accelerometer/esp32/src/main.cpp
+++ software/examples/08_bluetooth_with_accelerometer/esp32/src/main.cpp
@@ -7,7 +7,7 @@
 
 /***
  * Morgan Kenyon
- * Trying out example in platformIO, example 8
+ * example 8, sending sensor data over bluetooth
  * 
  */
 
@@ -25,11 +25,6 @@ Adafruit_MPU6050 mpu;
 
 float num;
 
-// Timer variables
-//unsigned long lastTime = 0;
-//unsigned long lastWriteOperation = 0;
-//unsigned long timerDelay = 3000;
-
 bool deviceConnected = false;
 bool oldDeviceConnected = false;//added
 
@@ -37,45 +32,26 @@ bool oldDeviceConnected = false;//added
 // https://www.uuidgenerator.net/
 #define SERVICE_UUID "91bad492-b950-4226-aa2b-4ede9fa42f59"
 
-BLEServer *pServer = NULL;//added
-BLEService *bmeService = NULL;
+BLEServer *bleServer = NULL;//added
+BLEService *oddService = NULL;
 
 // Number Characteristic and Descriptor
-BLECharacteristic bmeNumberCharacteristics(
+BLECharacteristic sensorCharacteristics(
   "cba1d466-344c-4be3-ab3f-189f80dd7518", 
-  BLECharacteristic::PROPERTY_NOTIFY | 
-  BLECharacteristic::PROPERTY_WRITE);
-BLEDescriptor bmeNumberDescriptor(BLEUUID((uint16_t)0x2902));
-
-//https://stackoverflow.com/a/75943324
-static void my_gatts_event_handler(esp_gatts_cb_event_t event, esp_gatt_if_t gatts_if, esp_ble_gatts_cb_param_t* param) {
-    // Do stuff depending on event type
-    // Eg. 'listen' to GATT events in realtime (very star trek).
-    // tone(2000 + event*200, volume, delay);
-    
-    Serial.println("event");
-}
+  BLECharacteristic::PROPERTY_NOTIFY);
+BLEDescriptor sensorDescriptor(BLEUUID((uint16_t)0x2902));
 
 //Setup callbacks onConnect and onDisconnect
 class MyServerCallbacks: public BLEServerCallbacks {
-  void onConnect(BLEServer* pServer) {
+  void onConnect(BLEServer* bleServer) {
     Serial.println("connected");
     deviceConnected = true;
   };
-  void onDisconnect(BLEServer* pServer) {
+  void onDisconnect(BLEServer* bleServer) {
     Serial.println("disconnected");
     deviceConnected = false;
   }
 };
-
-// class CharacterCallbacks: public BLECharacteristicCallbacks {
-//   void onWrite(BLECharacteristic* pCharacteristic) {
-//       std::string rxValue = pCharacteristic->getValue();
-//       Serial.print("value received = ");
-//       Serial.println(rxValue.c_str());
-//       //lastWriteOperation = millis();
-//   }
-// };
 
 //https://forum.arduino.cc/t/esp32-ble-does-not-disconnect/957235
 void checkToReconnect() //added
@@ -83,7 +59,7 @@ void checkToReconnect() //added
   // disconnected so advertise
   if (!deviceConnected && oldDeviceConnected) {
     delay(500); // give the bluetooth stack the chance to get things ready
-    pServer->startAdvertising(); // restart advertising
+    bleServer->startAdvertising(); // restart advertising
     Serial.println("Disconnected: start advertising");
     oldDeviceConnected = deviceConnected;
   }
@@ -95,6 +71,66 @@ void checkToReconnect() //added
   }
 }
 
+//prints out how the mpu sensor is setup
+void printAccelerometerStats()
+{
+  Serial.print("Accelerometer range set to: ");
+  switch (mpu.getAccelerometerRange()) {
+    case MPU6050_RANGE_2_G:
+      Serial.println("+-2G");
+      break;
+    case MPU6050_RANGE_4_G:
+      Serial.println("+-4G");
+      break;
+    case MPU6050_RANGE_8_G:
+      Serial.println("+-8G");
+      break;
+    case MPU6050_RANGE_16_G:
+      Serial.println("+-16G");
+      break;
+  }
+
+  Serial.print("Gyro range set to: ");
+  switch (mpu.getGyroRange()) {
+    case MPU6050_RANGE_250_DEG:
+      Serial.println("+- 250 deg/s");
+      break;
+    case MPU6050_RANGE_500_DEG:
+      Serial.println("+- 500 deg/s");
+      break;
+    case MPU6050_RANGE_1000_DEG:
+      Serial.println("+- 1000 deg/s");
+      break;
+    case MPU6050_RANGE_2000_DEG:
+      Serial.println("+- 2000 deg/s");
+      break;
+  }
+
+  Serial.print("Filter bandwidth set to: ");
+  switch (mpu.getFilterBandwidth()) {
+    case MPU6050_BAND_260_HZ:
+      Serial.println("260 Hz");
+      break;
+    case MPU6050_BAND_184_HZ:
+      Serial.println("184 Hz");
+      break;
+    case MPU6050_BAND_94_HZ:
+      Serial.println("94 Hz");
+      break;
+    case MPU6050_BAND_44_HZ:
+      Serial.println("44 Hz");
+      break;
+    case MPU6050_BAND_21_HZ:
+      Serial.println("21 Hz");
+      break;
+    case MPU6050_BAND_10_HZ:
+      Serial.println("10 Hz");
+      break;
+    case MPU6050_BAND_5_HZ:
+      Serial.println("5 Hz");
+      break;
+  }
+}
 void setup() {
   // Start serial communication 
   Serial.begin(115200);
@@ -111,98 +147,37 @@ void setup() {
 
   //accelerometer setup
   mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
-  Serial.print("Accelerometer range set to: ");
-  switch (mpu.getAccelerometerRange()) {
-  case MPU6050_RANGE_2_G:
-    Serial.println("+-2G");
-    break;
-  case MPU6050_RANGE_4_G:
-    Serial.println("+-4G");
-    break;
-  case MPU6050_RANGE_8_G:
-    Serial.println("+-8G");
-    break;
-  case MPU6050_RANGE_16_G:
-    Serial.println("+-16G");
-    break;
-  }
   mpu.setGyroRange(MPU6050_RANGE_500_DEG);
-  Serial.print("Gyro range set to: ");
-  switch (mpu.getGyroRange()) {
-  case MPU6050_RANGE_250_DEG:
-    Serial.println("+- 250 deg/s");
-    break;
-  case MPU6050_RANGE_500_DEG:
-    Serial.println("+- 500 deg/s");
-    break;
-  case MPU6050_RANGE_1000_DEG:
-    Serial.println("+- 1000 deg/s");
-    break;
-  case MPU6050_RANGE_2000_DEG:
-    Serial.println("+- 2000 deg/s");
-    break;
-  }
-
   mpu.setFilterBandwidth(MPU6050_BAND_5_HZ);
-  Serial.print("Filter bandwidth set to: ");
-  switch (mpu.getFilterBandwidth()) {
-  case MPU6050_BAND_260_HZ:
-    Serial.println("260 Hz");
-    break;
-  case MPU6050_BAND_184_HZ:
-    Serial.println("184 Hz");
-    break;
-  case MPU6050_BAND_94_HZ:
-    Serial.println("94 Hz");
-    break;
-  case MPU6050_BAND_44_HZ:
-    Serial.println("44 Hz");
-    break;
-  case MPU6050_BAND_21_HZ:
-    Serial.println("21 Hz");
-    break;
-  case MPU6050_BAND_10_HZ:
-    Serial.println("10 Hz");
-    break;
-  case MPU6050_BAND_5_HZ:
-    Serial.println("5 Hz");
-    break;
-  }
+  printAccelerometerStats();
 
   //bluetooth setup
-  // Create the BLE Device\
-  BLEDevice::setCustomGattsHandler(my_gatts_event_handler);
   BLEDevice::init(bleServerName);
 
   // Create the BLE Server
-  BLEServer *pServer = BLEDevice::createServer();
-  //*pServer = BLEDevice::createServer();
-  pServer->setCallbacks(new MyServerCallbacks());
+  BLEServer *bleServer = BLEDevice::createServer();
+  bleServer->setCallbacks(new MyServerCallbacks());
 
   // Create the BLE Service
-  BLEService *bmeService = pServer->createService(SERVICE_UUID);
+  BLEService *oddService = bleServer->createService(SERVICE_UUID);
 
   // Create BLE Characteristics and Create a BLE Descriptor
   // Number
-  bmeService->addCharacteristic(&bmeNumberCharacteristics);
-  bmeNumberDescriptor.setValue("BME Number");
-  bmeNumberCharacteristics.addDescriptor(&bmeNumberDescriptor);
-  //bmeNumberCharacteristics.setCallbacks(new CharacterCallbacks());
+  oddService->addCharacteristic(&sensorCharacteristics);
+  sensorDescriptor.setValue("BME Number");
+  sensorCharacteristics.addDescriptor(&sensorDescriptor);
 
-  
   // Start the service
-  bmeService->start();
+  oddService->start();
 
   // Start advertising
   BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
   pAdvertising->addServiceUUID(SERVICE_UUID);
-  pServer->getAdvertising()->start();
+  bleServer->getAdvertising()->start();
   Serial.println("Waiting a client connection to notify...");
 }
 
 void loop() {
-  checkToReconnect();
-  
   if (deviceConnected) {
     //read accelerometer data
     sensors_event_t a, g, temp;
@@ -229,23 +204,8 @@ void loop() {
 
     sprintf(sensorBuffer, "Acc:%s,%s,%s, Rot:%s,%s,%s", accX, accY, accZ, rotX, rotY, rotZ);
 
-    bmeNumberCharacteristics.setValue(sensorBuffer);
-    bmeNumberCharacteristics.notify();
-
-    // //old notify code
-    // num = num + 1;
-
-    // //Notify number reading
-    // static char numberTemp[6];
-    // dtostrf(num, 6, 2, numberTemp);
-    // //Set Number Characteristic value and notify connected client
-    // // bmeNumberCharacteristics.setValue(numberTemp);
-    // // bmeNumberCharacteristics.notify();
-    // Serial.print("Number: ");
-    // Serial.println(num);
-    
-    
-    // lastTime = millis();
+    sensorCharacteristics.setValue(sensorBuffer);
+    sensorCharacteristics.notify();
   }
   else
   {
