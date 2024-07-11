80855871bd7b4ac6e07ee3d4e06ee8361760fa69
Morgan Kenyon <morganwkenyon@gmail.com>
2024-07-10 20:54:33 -0500

    working on sense imu from platformio

---
+++ software/examples/13_nrf52_imu_platformio/README.md
@@ -0,0 +1,14 @@
+# The NRF52 IMU
+
+Now that we've got the blink working on the nrf52 Sense. I want to start working on using the accelerometer/gyro.
+
+## Issues
+
+* Failed to initialize IMU!
+  * When I first tried to run it, it didn't recognize the IMU
+
+## Resources
+
+* Forum Post - https://forum.seeedstudio.com/t/seeed-xiao-ble-nrf52840-sense-giving-bad-gyroscope-data/274134
+* SparkFun Documentation - https://learn.sparkfun.com/tutorials/lsm6ds3-breakout-hookup-guide/all#installing-the-arduino-library
+* Sparkfun Github Repo - https://github.com/sparkfun/SparkFun_LSM6DS3_Arduino_Library
---
+++ software/examples/13_nrf52_imu_platformio/nrf52/src/main.cpp
@@ -0,0 +1,39 @@
+/*
+Getting my IMU working inside the NRF52 Sense
+https://github.com/arduino-libraries/Arduino_LSM6DS3/blob/master/examples/SimpleAccelerometer/SimpleAccelerometer.ino
+*/
+#include "Arduino.h"
+#include <Arduino_LSM6DS3.h>
+#include "Wire.h"
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial);
+
+  if (!IMU.begin()) {
+    Serial.println("Failed to initialize IMU!");
+
+    while (1);
+  }
+
+  Serial.print("Accelerometer sample rate = ");
+  Serial.print(IMU.accelerationSampleRate());
+  Serial.println(" Hz");
+  Serial.println();
+  Serial.println("Acceleration in g's");
+  Serial.println("X\tY\tZ");
+}
+
+void loop() {
+  float x, y, z;
+
+  if (IMU.accelerationAvailable()) {
+    IMU.readAcceleration(x, y, z);
+
+    Serial.print(x);
+    Serial.print('\t');
+    Serial.print(y);
+    Serial.print('\t');
+    Serial.println(z);
+  }
+}
