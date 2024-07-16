a4fae5a76a6f7121f9c0d03da02896a72588e297
Morgan Kenyon <morganwkenyon@gmail.com>
2024-07-15 21:45:16 -0500

    Pushing string message to client

--- software/examples/15_nrf52_bluetooth/README.md
+++ software/examples/15_nrf52_bluetooth/README.md
@@ -8,6 +8,16 @@ After getting the accelerometer working in example 14, moving on to getting blue
 * Pulling in nrf52 code from resources below
 * As always, run and upload with `pio run --target upload`
 * For the csharp client, use `dotnet build` and `dotnet run` to run it once the nrf52 is running.
+  * After running, select the "NRF52" in the popup to proceed.
+  * You'll see the following messages:
+
+![printing from bluetooth](image-1.png)
+## Issues
+
+* If you put `while (!Serial);` in your `main.cpp` (or Arduino sketch), the program will not run until a Serial monitor is open.
+  * Maybe good for debugging, but took me awhile to figure out why it was only connecting with the serial monitor open.
+* Takes a while to connect (15-20 seconds) once the NRF52 has been identified.
+* Takes a while (20-30 seconds) for the Xiao Sense to receive disconnect messages.
 
 ## Resources
 
@@ -17,3 +27,4 @@ After getting the accelerometer working in example 14, moving on to getting blue
 * Other links
   * https://forum.seeedstudio.com/t/xiao-seeed-nrf52840-sense-sending-accelerometer-data-through-ble-not-working/275391/2
   * https://forum.arduino.cc/t/arduinoble-notify-issue-with-blecharacteristic/1062011/5
+  * https://github.com/arduino-libraries/ArduinoBLE/pull/5
--- software/examples/15_nrf52_bluetooth/nrf52/src/main.cpp
+++ software/examples/15_nrf52_bluetooth/nrf52/src/main.cpp
@@ -9,7 +9,7 @@
 
 BLEService nrf52Service("900e9509-a0b2-4d89-9bb6-b5e011e758b0");
 
-BLECharacteristic nrf52Characteristic("6ef4cd45-7223-43b2-b5c9-d13410b494f5", BLERead | BLEWrite | BLENotify | BLEIndicate, "nrf52Characteristic");
+BLEStringCharacteristic nrf52Characteristic("6ef4cd45-7223-43b2-b5c9-d13410b494f5", BLERead | BLEWrite | BLENotify | BLEIndicate, 20);
 BLEDescriptor nrf52Descriptor("57be2a28-fdf9-4cfd-8bbe-79a86d68765d","NRF52Descriptor");
 const int ledPin = LED_BUILTIN; // pin to use for the LED
 
@@ -30,7 +30,7 @@ void disconnectHandler(BLEDevice central) {
 
 void setup() {
   Serial.begin(125000);
-  while (!Serial);
+  //while (!Serial);
 
   // set LED pin to output mode
   pinMode(ledPin, OUTPUT);
@@ -65,35 +65,27 @@ void setup() {
 }
 
 void loop() {
-  Serial.println("Trying to listen");
+  //Serial.println("Trying to listen");
   BLEDevice central = BLE.central();
-  byte outgoing[8];
-  outgoing[0] = 0x44; //D
-  outgoing[1] = 0x61; //a
-  outgoing[2] = 0x74; //t
-  outgoing[3] = 0x61; //a
-  outgoing[4] = 0x44; //D
-  outgoing[5] = 0x69; //i
-  outgoing[6] = 0x73; //s
-  outgoing[7] = 0x63; //c
+  String value = "DataDisc";
 
   // if a central is connected to peripheral:
   if (central) {
-    Serial.print("Connected to central: ");
-    // print the central's MAC address:
-    Serial.println(central.address());
+    // Serial.print("Connected to central: ");
+    // // print the central's MAC address:
+    // Serial.println(central.address());
 
     // while the central is still connected to peripheral:
     while (central.connected()) {
-      Serial.println("writing value");
+      //Serial.println("writing value");
 
-      nrf52Characteristic.writeValue(outgoing, 8);
+      nrf52Characteristic.writeValue(value);
       delay(1000);
     }
 
     // when the central disconnects, print it out:
-    Serial.print(F("Disconnected from central: "));
-    Serial.println(central.address());
+    // Serial.print(F("Disconnected from central: "));
+    // Serial.println(central.address());
   }
 
   delay(3000);
