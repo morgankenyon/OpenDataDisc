3efc2285f2e1abdd88c8fcd0725a4339a569c868
Morgan Kenyon <morganwkenyon@gmail.com>
2024-10-27 20:46:47 -0500

    Continuing work on configuration dialog

--- software/client/OpenDataDisc.UI/ViewModels/ConfigurationWindowViewModel.cs
+++ software/client/OpenDataDisc.UI/ViewModels/ConfigurationWindowViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using InTheHand.Bluetooth;
+using OpenDataDisc.Services.Models;
 using OpenDataDisc.UI.Extensions;
 using ReactiveUI;
 using System;
@@ -10,7 +11,12 @@ namespace OpenDataDisc.UI.ViewModels
         public ConfigurationWindowViewModel()
         {
             this.WhenAnyValue(x => x.Step)
-                .Subscribe(_ => this.RaisePropertyChanged(nameof(StepText)));
+                .Subscribe(_ => {
+                    this.RaisePropertyChanged(nameof(StepText));
+                    this.RaisePropertyChanged(nameof(ConfigurationInstructionsText));
+                });
+            this.WhenAnyValue(x => x.ConfiguringValue)
+                .Subscribe(_ => this.RaisePropertyChanged(nameof(ConfiguringValueText)));
         }
 
         private int _messageCount;
@@ -27,8 +33,8 @@ namespace OpenDataDisc.UI.ViewModels
             set => this.RaiseAndSetIfChanged(ref _dataString, value);
         }
 
-        private float _configuringValue;
-        public float ConfiguringValue
+        private double? _configuringValue;
+        public double? ConfiguringValue
         {
             get => _configuringValue;
             set => this.RaiseAndSetIfChanged(ref _configuringValue, value);
@@ -42,6 +48,21 @@ namespace OpenDataDisc.UI.ViewModels
         }
 
         public string StepText => $"Config Step: {_step}";
+        public string ConfiguringValueText => ConfiguringValue.HasValue ? $"{Math.Round(ConfiguringValue.Value, 3)}" : "";
+        public string ConfigurationInstructionsText => Step switch
+        {
+            ConfigurationStep.Start => "Let's configure your OpenDataDisc. Please click Next when ready.",
+            ConfigurationStep.AccXSetup => "Please position device along the X axis, this value below should display close to 1",
+            ConfigurationStep.AccYSetup => "Please position device along the Y axis, this value below should display close to 1",
+            ConfigurationStep.AccZSetup => "Please position device along the Z axis, this value below should display close to 1",
+            ConfigurationStep.AccXRecording => "Recording, please don't touch device",
+            ConfigurationStep.AccYRecording => "Recording, please don't touch device",
+            ConfigurationStep.AccZRecording => "Recording, please don't touch device",
+            ConfigurationStep.GyroSetup => "Please lay the device flat and don't touch it",
+            ConfigurationStep.GyroRecording => "Recording, please don't touch device",
+            ConfigurationStep.Finished => "All finished, you can click closed to start throwing!",
+            _ => "Unknown, please reconnect your bluetooth device"
+        };
 
         public void HandleMessage(object? sender, GattCharacteristicValueChangedEventArgs e)
         {
@@ -51,11 +72,34 @@ namespace OpenDataDisc.UI.ViewModels
             {
                 MessageCount++;
                 DataString = data.ToString() ?? "";
+                ConfirmConfiguringValue(data);
             }
             else
             {
                 //TODO: log this somewhere more useful
                 Console.WriteLine(errorMessage);
+            }
+        }
+
+        private void ConfirmConfiguringValue(SensorData data)
+        {
+            switch (Step)
+            {
+                case ConfigurationStep.AccXSetup:
+                case ConfigurationStep.AccXRecording:
+                    ConfiguringValue = data.AccX;
+                    break;
+                case ConfigurationStep.AccYSetup:
+                case ConfigurationStep.AccYRecording:
+                    ConfiguringValue = data.AccY;
+                    break;
+                case ConfigurationStep.AccZSetup:
+                case ConfigurationStep.AccZRecording:
+                    ConfiguringValue = data.AccZ;
+                    break;
+                default:
+                    ConfiguringValue = null;
+                    break;
             }
         }
 
