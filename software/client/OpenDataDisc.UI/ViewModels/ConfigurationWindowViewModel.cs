e7b162bd6559602121967f90b2856193196c8229
Morgan Kenyon <morganwkenyon@gmail.com>
2024-10-27 20:10:42 -0500

    Displaying sensor data in configuration

--- software/client/OpenDataDisc.UI/ViewModels/ConfigurationWindowViewModel.cs
+++ software/client/OpenDataDisc.UI/ViewModels/ConfigurationWindowViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using InTheHand.Bluetooth;
+using OpenDataDisc.UI.Extensions;
 using ReactiveUI;
+using System;
 
 namespace OpenDataDisc.UI.ViewModels
 {
@@ -14,6 +16,13 @@ namespace OpenDataDisc.UI.ViewModels
             set => this.RaiseAndSetIfChanged(ref _messageCount, value);
         }
 
+        private string _dataString = string.Empty;
+        public string DataString
+        {
+            get => _dataString;
+            set => this.RaiseAndSetIfChanged(ref _dataString, value);
+        }
+
         private ConfigurationStep _step;
         public ConfigurationStep Step
         {
@@ -23,7 +32,18 @@ namespace OpenDataDisc.UI.ViewModels
 
         public void HandleMessage(object? sender, GattCharacteristicValueChangedEventArgs e)
         {
-            MessageCount++;
+            var (data, errorMessage) = e.ExtractSensorData();
+
+            if (data != null)
+            {
+                MessageCount++;
+                DataString = data.ToString() ?? "";
+            }
+            else
+            {
+                //TODO: log this somewhere more useful
+                Console.WriteLine(errorMessage);
+            }
         }
     }
 
