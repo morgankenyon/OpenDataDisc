b090ad61cd1a276302ad5390caf3464162808ece
Morgan Kenyon <morganwkenyon@gmail.com>
2024-10-30 23:30:31 -0500

    Adding streaming plot example

--- software/client/OpenDataDisc.UI/Views/MainWindow.axaml.cs
+++ software/client/OpenDataDisc.UI/Views/MainWindow.axaml.cs
@@ -1,16 +1,25 @@
 using Avalonia.Controls;
 using Avalonia.ReactiveUI;
+using Avalonia.Threading;
 using OpenDataDisc.Services.Models;
 using OpenDataDisc.UI.Models;
 using OpenDataDisc.UI.ViewModels;
 using ReactiveUI;
 using ScottPlot.Avalonia;
+using ScottPlot.Plottables;
+using System;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace OpenDataDisc.UI.Views;
 
 public partial class MainWindow : ReactiveWindow<MainWindowViewModel>
 {
+    readonly ScottPlot.Plottables.DataStreamer Streamer1;
+    readonly ScottPlot.DataGenerators.RandomWalker Walker1 = new(0);
+    readonly ScottPlot.Plottables.VerticalLine VLine;
+    private DispatcherTimer _addNewDataTimer;
+    private DispatcherTimer _updatePlotTimer;
     public MainWindow()
     {
         InitializeComponent();
@@ -28,8 +37,57 @@ public partial class MainWindow : ReactiveWindow<MainWindowViewModel>
         double[] dataY = { 1, 4, 9, 16, 25, 36, 49, 64, 81, 100 };
 
         AvaPlot avaPlot1 = this.Find<AvaPlot>("AvaPlot1");
-        avaPlot1.Plot.Add.Scatter(dataX, dataY);
-        avaPlot1.Refresh();
+        //avaPlot1.Plot.Add.Scatter(dataX, dataY);
+        //avaPlot1.Refresh();
+
+        var Streamer1 = avaPlot1.Plot.Add.DataStreamer(1000);
+        VLine = avaPlot1.Plot.Add.VerticalLine(0, 2, ScottPlot.Colors.Red);
+
+        // disable mouse interaction by default
+        avaPlot1.UserInputProcessor.Disable();
+
+        // only show marker button in scroll mode
+        //btnMark.Visible = false;
+        _addNewDataTimer = new DispatcherTimer
+        {
+            Interval = TimeSpan.FromMilliseconds(10)
+        };
+        _addNewDataTimer.Tick += (s, e) =>
+        {
+            int count = 5;
+
+            // add new sample data
+            var nextValue = Walker1.Next(count);
+            Streamer1.AddRange(nextValue);
+
+            // slide marker to the left
+            avaPlot1.Plot.GetPlottables<Marker>()
+                .ToList()
+                .ForEach(m => m.X -= count);
+
+            // remove off-screen marks
+            avaPlot1.Plot.GetPlottables<Marker>()
+                .Where(m => m.X < 0)
+                .ToList()
+                .ForEach(m => avaPlot1.Plot.Remove(m));
+        };
+        _addNewDataTimer.Start();
+
+        _updatePlotTimer = new DispatcherTimer
+         {
+             Interval = TimeSpan.FromMilliseconds(50)
+         };
+        _updatePlotTimer.Tick += (s, e) =>
+         {
+             if (Streamer1.HasNewData)
+             {
+                 avaPlot1.Plot.Title($"Processed {Streamer1.Data.CountTotal:N0} points");
+                 VLine.IsVisible = Streamer1.Renderer is ScottPlot.DataViews.Wipe;
+                 VLine.Position = Streamer1.Data.NextIndex * Streamer1.Data.SamplePeriod + Streamer1.Data.OffsetX;
+                 avaPlot1.Refresh();
+             }
+         };
+        _updatePlotTimer.Start();
     }
 
     private async Task DoShowBluetoothSelectorAsync(
